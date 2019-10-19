50ef8c90356eeeb364151751b325a4637768bbf9
Michael Copeland <cookiefries12@gmail.com>
2019-10-19 18:40:12 -0500

    Add powerups and game resizing

--- script.js
+++ script.js
@@ -4,12 +4,18 @@ const ctx = canvas.getContext("2d");
 const divElem = document.getElementById("btn-wrapper");
 const buttonElem = document.getElementById("play-btn");
 const colors = {
-  snakeFill: "green",
-  snakeOutline: "darkgreen",
-  appleFill: "red",
-  appleOutline: "darkred",
-  gameFill: "white",
-  gameOutline: "black"
+  snakeFill: "hsla(114, 80%, 60%, 1)",
+  snakeOutline: "hsla(114, 80%, 20%, 1)",
+  appleFill: "hsla(0, 80%, 60%, 1)",
+  appleOutline: "hsla(0, 80%, 20%, 1)",
+  gameFill: "hsla(0, 0%, 100%, 1)",
+  gameOutline: "hsla(0, 0%, 0%, 1)",
+  speedupFill: "hsla(240, 80%, 60%, 1)",
+  speedupOutline: "hsla(240, 80%, 20%, 1)",
+  slowdownFill: "hsla(60, 80%, 60%, 1)",
+  slowdownOutline: "hsla(60, 80%, 20%, 1)",
+  doublePointsFill: "hsla(330, 80%, 60%, 1)",
+  doublePointsOutline: "hsla(330, 80%, 20%, 1)"
 }
 let speed = 100;
 let snake = [{
@@ -34,10 +40,25 @@ let snake = [{
   }
 ]
 let scoreNum = 0;
+let highScoreNum = 0;
 let score = document.querySelector("#score");
+let highScore = document.querySelector("#high-score");
 let turning = false;
-let key;
+let points = 5;
+let key, key1, key2, key3;
 let apple = {
+  x: 0,
+  y: 0
+}
+let speedup = {
+  x: 0,
+  y: 0
+}
+let slowdown = {
+  x: 0,
+  y: 0
+}
+let doublePoints = {
   x: 0,
   y: 0
 }
@@ -53,7 +74,7 @@ function resetDisplay() {
   ctx.fillStyle = colors.gameFill;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
   ctx.strokeRect(0, 0, canvas.width, canvas.height);
-};
+}
 resetDisplay();
 
 function showApple() {
@@ -61,7 +82,28 @@ function showApple() {
   ctx.fillStyle = colors.appleFill;
   ctx.fillRect(apple.x, apple.y, 10, 10);
   ctx.strokeRect(apple.x, apple.y, 10, 10);
-};
+}
+
+function showSpeedup() {
+  ctx.strokeStyle = colors.speedupOutline;
+  ctx.fillStyle = colors.speedupFill;
+  ctx.fillRect(speedup.x, speedup.y, 10, 10);
+  ctx.strokeRect(speedup.x, speedup.y, 10, 10);
+}
+
+function showSlowdown() {
+  ctx.strokeStyle = colors.slowdownOutline;
+  ctx.fillStyle = colors.slowdownFill;
+  ctx.fillRect(slowdown.x, slowdown.y, 10, 10);
+  ctx.strokeRect(slowdown.x, slowdown.y, 10, 10);
+}
+
+function showDoublePoints() {
+  ctx.strokeStyle = colors.doublePointsOutline;
+  ctx.fillStyle = colors.doublePointsFill;
+  ctx.fillRect(doublePoints.x, doublePoints.y, 10, 10);
+  ctx.strokeRect(doublePoints.x, doublePoints.y, 10, 10);
+}
 
 function snakeMove() {
   let head = {
@@ -70,12 +112,43 @@ function snakeMove() {
   }
   snake.unshift(head);
   let ateFood = (snake[0].x === apple.x && snake[0].y === apple.y) ? true : false;
+  let gotSpeedup = (snake[0].x === speedup.x && snake[0].y === speedup.y) ? true : false;
+  let gotSlowdown = (snake[0].x === slowdown.x && snake[0].y === slowdown.y) ? true : false;
+  let gotDoublePoints = (snake[0].x === doublePoints.x && snake[0].y === doublePoints.y) ? true : false;
   if (ateFood) {
-    scoreNum += 10;
+    scoreNum += points * 2;
     score.innerHTML = scoreNum;
     newApple();
   } else {
     snake.pop();
+  }
+  if (gotSpeedup) {
+    scoreNum += points;
+    score.innerHTML = scoreNum;
+    speed -= 5;
+    hidePowerup(speedup);
+    key1 = setTimeout(() => {
+      spawnSpeedup();
+    }, 10000)
+  }
+  if (gotSlowdown) {
+    scoreNum += points;
+    score.innerHTML = scoreNum;
+    speed += 5;
+    hidePowerup(slowdown);
+    key2 = setTimeout(() => {
+      spawnSlowdown();
+    }, 10000)
+  }
+  if (gotDoublePoints) {
+    scoreNum += points;
+    score.innerHTML = scoreNum;
+    points = points * 2;
+    hidePowerup(doublePoints);
+    key3 = setTimeout(() => {
+      points = points / 2;
+      spawnDoublePoints();
+    }, 10000)
   }
 }
 
@@ -91,14 +164,89 @@ function crashed() {
   return false;
 }
 
+function randomTen(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+}
+
 function newApple() {
-  apple.x = Math.round((Math.random() * canvas.width - 10) / 10) * 10;
-  apple.y = Math.round((Math.random() * canvas.height - 10) / 10) * 10;
+  apple.x = randomTen(0, canvas.width - 10);
+  apple.y = randomTen(0, canvas.height - 10);
   snake.forEach(part => {
     if (part.x === apple.x && part.y === apple.y) {
       newApple();
     }
   })
+  if (apple.x === speedup.x && apple.y === speedup.y) {
+    newApple();
+  }
+  if (apple.x === slowdown.x && apple.y === slowdown.y) {
+    newApple();
+  }
+  if (apple.x === doublePoints.x && apple.y === doublePoints.y) {
+    newApple();
+  }
+}
+
+function spawnSpeedup() {
+  speedup.x = randomTen(0, canvas.width - 10);
+  speedup.y = randomTen(0, canvas.height - 10);
+  snake.forEach(part => {
+    if (part.x === speedup.x && part.y === speedup.y) {
+      spawnSpeedup();
+    }
+  })
+  if (apple.x === speedup.x && apple.y === speedup.y) {
+    spawnSpeedup();
+  }
+  if (slowdown.x === speedup.x && slowdown.y === speedup.y) {
+    spawnSpeedup();
+  }
+  if (speedup.x === doublePoints.x && speedup.y === doublePoints.y) {
+    spawnSpeedup();
+  }
+}
+
+function spawnSlowdown() {
+  slowdown.x = randomTen(0, canvas.width - 10);
+  slowdown.y = randomTen(0, canvas.height - 10);
+  snake.forEach(part => {
+    if (part.x === slowdown.x && part.y === slowdown.y) {
+      spawnSlowdown();
+    }
+  })
+  if (apple.x === slowdown.x && apple.y === slowdown.y) {
+    spawnSlowdown();
+  }
+  if (slowdown.x === speedup.x && slowdown.y === speedup.y) {
+    spawnSlowdown();
+  }
+  if (slowdown.x === doublePoints.x && slowdown.y === doublePoints.y) {
+    spawnSlowdown();
+  }
+}
+
+function spawnDoublePoints() {
+  doublePoints.x = randomTen(0, canvas.width - 10);
+  doublePoints.y = randomTen(0, canvas.height - 10);
+  snake.forEach(part => {
+    if (part.x === doublePoints.x && part.y === doublePoints.y) {
+      spawnDoublePoints();
+    }
+  })
+  if (slowdown.x === doublePoints.x && slowdown.y === doublePoints.y) {
+    spawnDoublePoints();
+  }
+  if (speedup.x === doublePoints.x && speedup.y === doublePoints.y) {
+    spawnDoublePoints();
+  }
+  if (apple.x === doublePoints.x && apple.y === doublePoints.y) {
+    spawnDoublePoints();
+  }
+}
+
+function hidePowerup(powerup) {
+  powerup.x = -100;
+  powerup.y = -100;
 }
 
 function showSnake() {
@@ -117,16 +265,16 @@ function turn(event) {
   turning = true;
   let pressed = event.keyCode;
   // 1) Left 2) Up 3) Right 4) Down
-  if (pressed === 37 && velocity.x !== 10) {
+  if ((pressed === 37 || pressed === 65) && velocity.x !== 10) {
     velocity.x = -10;
     velocity.y = 0;
-  } else if (pressed === 38 && velocity.y !== 10) {
+  } else if ((pressed === 38 || pressed === 87) && velocity.y !== 10) {
     velocity.x = 0;
     velocity.y = -10;
-  } else if (pressed === 39 && velocity.x !== -10) {
+  } else if ((pressed === 39 || pressed === 68) && velocity.x !== -10) {
     velocity.x = 10;
     velocity.y = 0;
-  } else if (pressed === 40 && velocity.y !== -10) {
+  } else if ((pressed === 40 || pressed === 83) && velocity.y !== -10) {
     velocity.x = 0;
     velocity.y = 10;
   }
@@ -141,6 +289,9 @@ function play() {
     turning = false;
     resetDisplay();
     showApple();
+    showSpeedup();
+    showSlowdown();
+    showDoublePoints();
     snakeMove();
     showSnake();
     play();
@@ -149,14 +300,36 @@ function play() {
 
 function start() {
   divElem.classList.add("hide");
+  const smallSize = document.getElementById("small");
+  const mediumSize = document.getElementById("medium");
+  const largeSize = document.getElementById("large");
+  if (smallSize.checked) {
+    canvas.width = 300;
+    canvas.height = 300;
+  } else if (mediumSize.checked) {
+    canvas.width = 600;
+    canvas.height = 600;
+  } else if (largeSize.checked) {
+    canvas.width = 900;
+    canvas.height = 900;
+  }
   play();
   newApple();
+  spawnSpeedup();
+  spawnSlowdown();
+  spawnDoublePoints();
+  scoreNum = 0;
+  score.innerHTML = scoreNum;
 }
 
 function end() {
   clearTimeout(key);
+  clearTimeout(key1);
+  clearTimeout(key2);
+  clearTimeout(key3);
   divElem.classList.remove("hide");
   speed = 100;
+  points = 5;
   snake = [{
       x: 150,
       y: 150
@@ -178,9 +351,24 @@ function end() {
       y: 150
     }
   ]
-  scoreNum = 0;
+  if (scoreNum > highScoreNum) {
+    highScoreNum = scoreNum;
+    highScore.innerHTML = highScoreNum;
+  }
   turning = false;
   apple = {
+    x: 0,
+    y: 0
+  }
+  speedup = {
+    x: 0,
+    y: 0
+  }
+  slowdown = {
+    x: 0,
+    y: 0
+  }
+  doublePoints = {
     x: 0,
     y: 0
   }
